27fbc3f631d66ad5a225e433828ea72e5439a9d3
Baptiste Rimetz <baptiste.rimetz@outlook.fr>
2025-08-25 11:12:40 +0200

    fix some warning

--- ArdaCore/Core.h
+++ ArdaCore/Core.h
@@ -8,8 +8,8 @@ using int8 = signed char;
 using uint8 = unsigned char;
 using int16 = signed short;
 using uint16 = unsigned short;
-using int32 = signed long;
-using uint32 = unsigned long;
+using int32 = signed int;
+using uint32 = unsigned int;
 using int64 = signed long long;
 using uint64 = unsigned long long;
 
