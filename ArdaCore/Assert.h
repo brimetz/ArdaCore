6a80eb8eade3f63a785abe5e0766a798bca595c5
Baptiste Rimetz <baptiste.rimetz@outlook.fr>
2025-08-25 10:29:24 +0200

    Add specific case for BREAKPOINT macro when on linux

--- ArdaCore/Assert.h
+++ ArdaCore/Assert.h
@@ -1,6 +1,11 @@
 #pragma once
 
+#ifdef __LINUX__
+#define BREAKPOINT
+#endif
+#ifdef _WIN32
 #define BREAKPOINT __debugbreak()
+#endif
 
 #define Assert(condition)																\
 	do                                                                                  \
